c0125897c8da85e0341678dbd3ff21b17f094205
jungdaekim <tojdkim@gmail.com>
2024-10-10 23:02:35 +0900

    Add files via upload
    참조: https://www.youtube.com/watch?v=A8DwSlQI4Y8&t=651s

---
+++ WebCamBarcode/WebCamBarcode/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WebCamBarcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(713, 609);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(748, 89);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 78);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(748, 331);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(126, 78);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 649);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ WebCamBarcode/WebCamBarcode/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.Barcode;
+using BasselTech_CamCapture;
+
+namespace WebCamBarcode
+{
+    public partial class Form1 : Form
+    {
+        Camera cam;
+        Timer t;
+        BackgroundWorker worker;
+        Bitmap CapImage;
+        public Form1()
+        {
+            InitializeComponent();
+
+            t = new Timer();
+            cam = new Camera(pictureBox1);
+            worker = new BackgroundWorker();
+
+            worker.DoWork += Worker_DoWork;
+            t.Tick += T_Tick;
+            t.Interval = 1;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            CapImage = cam.GetBitmap();
+            if (CapImage != null && !worker.IsBusy)
+                worker.RunWorkerAsync();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BarcodeDecoder decoder = new BarcodeDecoder();
+
+            try
+            {
+                string decoded_text = decoder.Decode(CapImage).Text;
+                MessageBox.Show(decoded_text);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cam.Start();
+                t.Start();
+                btnStop.Enabled = true;
+                btnStart.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                cam.Stop();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+            cam.Stop();
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+        }
+    }
+}
